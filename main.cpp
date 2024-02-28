f233f40dbab45d528647eeb166340c934bb1d26b
Ramin Akbari <akbarigh@msu.edu>
2024-02-28 03:43:03 -0500

    updated main file. Now it is a benchmark for matrix-matrix multiplication.

--- main.cpp
+++ main.cpp
@@ -3,19 +3,15 @@
 #include <iostream>
 
 int main() {
-  // Matrix a(3000, 3000, true);
-  // Matrix b(3000, 3000, true);
-  // auto st = std::chrono::high_resolution_clock::now();
-  // a.transpose();
-  // auto end = std::chrono::high_resolution_clock::now();
-  // auto c = a * b;
-  // auto elapsed =
-  //     std::chrono::duration_cast<std::chrono::microseconds>(end - st);
-  // std::cout << elapsed.count() / 1e3 << "ms \n";
-  // std::cout << c(10, 15) << "\n";
-  Matrix a(3, 5, true);
-  (a >> std::cout) << "\n";
+  Matrix a(3000, 3000, true);
+  Matrix b(3000, 3000, true);
+  auto st = std::chrono::high_resolution_clock::now();
   a.transpose();
-  (a >> std::cout) << "\n";
+  auto end = std::chrono::high_resolution_clock::now();
+  auto c = a * b;
+  auto elapsed =
+      std::chrono::duration_cast<std::chrono::microseconds>(end - st);
+  std::cout << elapsed.count() / 1e3 << "ms \n";
+  std::cout << c(10, 15) << "\n";
   return 0;
 }
