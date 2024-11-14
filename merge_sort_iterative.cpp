b3f927f0e6e50e350267acaa94d9021156603a3c
Ramin Akbari <akbarigh@msu.edu>
2024-11-14 18:13:19 -0500

    added insertion sort to merge sort (hybrid sort)

--- merge_sort_iterative.cpp
+++ merge_sort_iterative.cpp
@@ -9,6 +9,19 @@ unsigned long log2_rem(unsigned long x,unsigned long rem = 1){
     return count;
 }
 
+void insertion_sort(std::vector<int>& a,unsigned long left,unsigned long right){
+    unsigned long ptr = left+1;
+    unsigned long index;
+    while(ptr <= right){
+        index = ptr;
+        while(index>left && a[index]<a[index-1]){
+            std::swap(a[index],a[index-1]);
+            --index;
+        }
+        ++ptr;
+    }
+}
+
 void merge(std::vector<int> &arr_read,std::vector<int>& arr_write, unsigned long left ,unsigned long mid ,unsigned long right){
     unsigned long ptr_left = left;
     unsigned long ptr_right = mid+1;
@@ -29,22 +42,28 @@ void merge(std::vector<int> &arr_read,std::vector<int>& arr_write, unsigned long
         arr_write[index++] = arr_read[ptr_right++];
 }
 
-void merge_sort(std::vector<int>& arr_read,unsigned long window = 2){
+void merge_sort(std::vector<int>& arr_read, unsigned long window = 45){
     std::vector<int> arr_write(arr_read.size());
-    auto n = log2_rem(arr_read.size());
-    unsigned long mid = window / 2;
+    auto n = log2_rem(arr_read.size(),window);
+    unsigned long left = 0;
+    while (left + window <= arr_read.size()){
+        insertion_sort(arr_read,left,left+window-1);
+        left += window;
+    }
+    insertion_sort(arr_read,left,arr_read.size()-1);
+    unsigned long merged_size = window*2;
     for(unsigned long i=0;i<n;++i){
-        unsigned long left = 0;
-        while (left + window <= arr_read.size()){
-            merge(arr_read,arr_write,left,left+mid-1,left+window-1);
-            left += window;
+        left = 0;
+        while (left + merged_size <= arr_read.size()){
+            merge(arr_read,arr_write,left,left+window-1,left+merged_size-1);
+            left += merged_size;
         }
         if (arr_read.size()-left > 0){
-            merge(arr_read,arr_write,left,std::min(left+mid-1,arr_read.size()-1),arr_read.size()-1);
+            merge(arr_read,arr_write,left,std::min(left+window-1,arr_read.size()-1),arr_read.size()-1);
         }
         std::swap(arr_read,arr_write);
         window *= 2;
-        mid *= 2;
+        merged_size *= 2;
     }
     if((n&1)==1)
         std::copy(arr_read.begin(),arr_read.end(),arr_write.begin());
@@ -56,17 +75,23 @@ int main(){
     // Initialize a random number generator
     std::random_device rd;
     std::mt19937 gen(rd());
-    int n = 10000000;
+    int n = 100000;
 
     // Define a distribution range for integers
-    std::uniform_int_distribution<int> dist(1, 10000);
+    std::uniform_int_distribution<int> dist(1, n);
 
     std::vector<int> a(n);
     for (auto &val:a){
         val = dist(gen);
     }
-
+    std::vector<int> b = a;
+    std::sort(b.begin(), b.end());
     merge_sort(a);
+
+    for (int i=0;i<n;i++){
+        if(a[i]!=b[i])
+            std::cout<<"wrong\n";
+    }
 
 
     return 0;
