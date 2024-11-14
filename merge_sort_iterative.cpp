93a04aac66adf481e1de91965e973d52386f018b
Ramin Akbari <akbarigh@msu.edu>
2024-11-14 15:55:56 -0500

    fixed a bug, out of bound indexing

--- merge_sort_iterative.cpp
+++ merge_sort_iterative.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <random>
 
 unsigned long log2_rem(unsigned long x,unsigned long rem = 1){
     unsigned long count=0;
@@ -28,11 +29,10 @@ void merge(std::vector<int> &arr_read,std::vector<int>& arr_write, unsigned long
         arr_write[index++] = arr_read[ptr_right++];
 }
 
-void merge_sort(std::vector<int>& arr_read){
+void merge_sort(std::vector<int>& arr_read,unsigned long window = 2){
     std::vector<int> arr_write(arr_read.size());
     auto n = log2_rem(arr_read.size());
-    unsigned long window = 2;
-    unsigned long mid = 1;
+    unsigned long mid = window / 2;
     for(unsigned long i=0;i<n;++i){
         unsigned long left = 0;
         while (left + window <= arr_read.size()){
@@ -40,7 +40,7 @@ void merge_sort(std::vector<int>& arr_read){
             left += window;
         }
         if (arr_read.size()-left > 0){
-            merge(arr_read,arr_write,left,left+mid-1,arr_read.size()-1);
+            merge(arr_read,arr_write,left,std::min(left+mid-1,arr_read.size()-1),arr_read.size()-1);
         }
         std::swap(arr_read,arr_write);
         window *= 2;
@@ -53,11 +53,21 @@ void merge_sort(std::vector<int>& arr_read){
 
 int main(){
 
-    std::vector<int> a={11,4,6,9,17,8,1,3,5,2,13,7};
-    merge_sort(a);
-    for (auto val:a){
-        std::cout << val << "\n";
+    // Initialize a random number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    int n = 10000000;
+
+    // Define a distribution range for integers
+    std::uniform_int_distribution<int> dist(1, 10000);
+
+    std::vector<int> a(n);
+    for (auto &val:a){
+        val = dist(gen);
     }
+
+    merge_sort(a);
+
 
     return 0;
 }
